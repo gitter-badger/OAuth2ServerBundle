a147d04f05417b557b6b5674a7fb84b337e2f607
Florent Morselli <florent+git@morselli.fr>
2015-08-20 13:19:16 +0200

    Minor changes

--- Plugin/CorePlugin/CorePlugin.php
+++ Plugin/CorePlugin/CorePlugin.php
@@ -6,7 +6,6 @@ use Matthias\BundlePlugins\BundlePlugin;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
-use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 
 class CorePlugin implements BundlePlugin
 {
@@ -25,12 +24,6 @@ class CorePlugin implements BundlePlugin
 
     public function build(ContainerBuilder $container)
     {
-        /*$mappings = array(
-            realpath(__DIR__ . '/Resources/config/doctrine-mapping') => 'SpomkyLabs\OAuth2ServerBundle\Plugin\AuthCodeGrantTypePlugin\Model',
-        );
-        if (class_exists('Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass')) {
-            $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver($mappings, array('oauth2_server.auth_code.manager')));
-        }*/
     }
 
     public function boot(ContainerInterface $container)
