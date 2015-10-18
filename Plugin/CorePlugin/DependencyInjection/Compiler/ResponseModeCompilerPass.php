9fa5c9f60a05cfda95cddaca2d19589dd884e7ce
Florent Morselli <florent+git@morselli.fr>
2015-10-18 23:22:24 +0200

    Response mode compiler pass

---
+++ Plugin/CorePlugin/DependencyInjection/Compiler/ResponseModeCompilerPass.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SpomkyLabs\OAuth2ServerBundle\Plugin\CorePlugin\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class ResponseModeCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('oauth2_server.authorization_endpoint')) {
+            return;
+        }
+
+        $definition = $container->getDefinition('oauth2_server.authorization_endpoint');
+
+        $taggedServices = $container->findTaggedServiceIds('oauth2_server.response_mode');
+        foreach ($taggedServices as $id => $attributes) {
+            $definition->addMethodCall('addResponseMode', [new Reference($id)]);
+        }
+    }
+}
